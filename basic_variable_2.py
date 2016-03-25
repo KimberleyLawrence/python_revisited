f92ff15e475cf4ea15426005c43ec8dfd24dcd11
KimberleyLawrence <kimberleynedic@gmail.com>
2016-03-24 20:06:59 -0700

    basic variables - order is important
    This code will only print number 3 not number 7 and number 3

---
+++ basic_variable_2.py
@@ -0,0 +1,13 @@
+# my_int is set to 7 below. What do you think
+# will happen if we reset it to 3 and print the result?
+
+my_int = 7
+
+# Change the value of my_int to 3 on line 8!
+# this one is taken as the most recent, so this code will only print 3 not 7 and 3
+my_int = 3
+
+# Here's some code that will print my_int to the console:
+# The print keyword will be covered in detail soon!
+
+print my_int
