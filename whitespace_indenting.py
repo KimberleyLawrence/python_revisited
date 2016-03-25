856a164b52a0fe260787e378f2190f639b6a015e
KimberleyLawrence <kimberleynedic@gmail.com>
2016-03-25 12:52:18 -0700

    Importance of whitespace/indentation
    In Python, whitespace is used to structure code. Whitespace is
    important, so you have to be careful with how you use it.
    Top code throws error:
    “IndentationError: expected an indented block”
    You'll get this error whenever your whitespace is off

---
+++ whitespace_indenting.py
@@ -0,0 +1,15 @@
+
+#Incorrect indenting - throws error
+def spam():
+eggs = 12
+return eggs
+
+print spam()
+
+
+#correct indenting (whitespace)
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
