c74bedef2b641654041d52b986e56ce9d37d36ee
KimberleyLawrence <kimberleynedic@gmail.com>
2016-04-06 21:10:29 -0700

    Creating a tip calculator

---
+++ tipping_calc_1.py
@@ -0,0 +1,10 @@
+# Assign the variable meal the value 44.50 on line 3!
+#You've finished eating at a restaurant, and received this bill:
+
+#Cost of meal: $44.50
+#Restaurant tax: 6.75%
+#Tip: 15%
+#You'll apply the tip to the overall cost of the meal (including tax).
+
+
+meal = 44.50
