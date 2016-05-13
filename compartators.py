6354128fa6dd085df289eaab58756348d71c1648
KimberleyLawrence <kimberleynedic@gmail.com>
2016-05-12 18:42:25 -0700

    Playing with comparitors

--- compartators.py
+++ compartators.py
@@ -13,4 +13,20 @@ bool_three = True
 bool_four = False
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
+bool_five = False# Assign True or False as appropriate on the lines below!
+
+# (20 - 10) > 15
+bool_one = False    # We did this one for you!
+
+# (10 + 17) == 3**16
+# Remember that ** can be read as 'to the power of'. 3**16 is about 43 million.
+bool_two = False
+
+# 1**2 <= -1
+bool_three = False
+
+# 40 * 4 >= -4
+bool_four = True
+
+# 100 != 10**2
 bool_five = False
