97afb60a12b8d066af159568146a92c784b07e8c
KimberleyLawrence <kimberleynedic@gmail.com>
2016-05-13 18:46:42 -0700

    This time adding in math to make the statement true of false

---
+++ comparisons_3.py
@@ -0,0 +1,16 @@
+# Create comparative statements as appropriate on the lines below!
+
+# Make me true!
+bool_one = 3 < 5  # We already did this one for you!
+
+# Make me false!
+bool_two = 234 < 1
+
+# Make me true!
+bool_three = 54 >= 50 + 4
+
+# Make me false!
+bool_four = 2 * 2 == 6
+
+# Make me true!
+bool_five = 5 <= 6
