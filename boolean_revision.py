f69f81dc3e17897f5a321a013a57ded065d1fb44
KimberleyLawrence <kimberleynedic@gmail.com>
2016-05-19 21:39:54 -0700

    Boolean revision

---
+++ boolean_revision.py
@@ -0,0 +1,16 @@
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = True and True
+
+# Make me false!
+bool_three = False or 3 * 3 == 4
+
+# Make me true!
+bool_four = 2+2 == 4 or 6+6 == 12
+
+# Make me true!
+bool_five = "Yes" == "Yes" or 3+3 == 6
