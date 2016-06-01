c88599b9fcae65dccd1286c09d98aefcdf00317b
KimberleyLawrence <kimberleynedic@gmail.com>
2016-05-31 22:58:07 -0700

    pyg latin making it work - dan helped

---
+++ pyg_latin_8.py
@@ -0,0 +1,15 @@
+pyg = 'ay'
+original = raw_input('Enter a word:')
+word = original.lower()
+first = word[0]
+truncated_word = word[1:]
+new_word = truncated_word + first + pyg
+print "#", truncated_word
+print "#", first
+print "#", original
+# GOAL: "hello" > "ellohay"
+
+if len(original) > 0 and original.isalpha():
+    print new_word
+else:
+    print 'empty'
