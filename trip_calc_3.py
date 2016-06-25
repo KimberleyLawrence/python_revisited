8ea12dd250a51557f06edf04618a4b140a893ab7
KimberleyLawrence <kimberleynedic@gmail.com>
2016-06-26 08:19:00 +1000

    Putting it all together
    Great! Now that you've got your 3 main costs figured out, let's put
    them together in order to find the total cost of your trip.
    
    def double(n):
        return 2 * n
    def triple(p):
        return 3 * p
    
    def add(a, b):
        return double(a) + triple(b)
    We define two simple functions, double(n) and triple(p) that return 2
    times or 3 times their input. Notice that they have n and p as their
    arguments.
    We define a third function, add(a, b) that returns the sum of the
    previous two functions when called with a and b, respectively.
    Instructions
    Below your existing code, define a function called trip_cost that takes
    two arguments, city and days.
    Like the example above, have your function return the sum of calling
    the rental_car_cost(days), hotel_cost(days), and plane_ride_cost(city)
    functions.
    It is completely valid to call the hotel_cost(nights) function with the
    variable days. Just like the example above where we call double(n) with
    the variable a, we pass the value of days to the new function in the
    argument nights.

---
+++ trip_calc_3.py
@@ -0,0 +1,24 @@
+def hotel_cost(nights):
+    hotel_cost = 140
+    return hotel_cost * nights
+
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    if city == "Tampa":
+        return 220
+    if city == "Pittsburgh":
+        return 222
+    if city == "Los Angeles":
+        return 475
+
+def rental_car_cost(days):
+    car_hire = 40 * days
+    if days >= 7:
+        car_hire -= 50
+    elif days >= 3:
+        car_hire -= 20
+    return car_hire
+
+def trip_cost(city, days):
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city)
